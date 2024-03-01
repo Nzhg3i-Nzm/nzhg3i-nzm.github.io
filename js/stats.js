438f2893eeab5b80e6ee21b5b7e503da8b12891e
Nzhg3i-Nzm <136181661+Nzhg3i-Nzm@users.noreply.github.com>
2024-03-01 10:27:06 -0600

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,5 +1,29 @@
-async function getJson(){
+async function getVulns(){
   const res = await fetch("../../vulns.json");
   const vulns = await res.json();
   console.log(vulns);
+  return vulns;
+}
+
+function createTable(){
+  var vulns = getVulns();
+  const v_obj = JSON.parse(vulns);
+  const keys = Object.keys(v_obj);
+  const values[];
+
+  for (key in keys){
+    //get all keys
+    values.push(v_obj.key);
+  }
+
+  for (key in keys){
+    var t = document.getElementById("table");
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML=keys[key];
+    var td2 = document.createElement("td");
+    td2.innerHTML=values[key];
+    tr.appendChild(td);
+    tr.appendChild(td2);
+    t.appendCHild(tr);
 }
