2471220fcf935b14acd57ca202fb9ab52c20e242
Nzhg3i-Nzm <136181661+Nzhg3i-Nzm@users.noreply.github.com>
2024-03-01 11:02:39 -0600

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -16,13 +16,13 @@ function createTable(){
     values.push(v_obj.key);
   }
 
-  for (key in keys){
+  while (var i=0; i<keys.length; i++){
     var t = document.getElementById("table");
     var tr = document.createElement("tr");
     var td = document.createElement("td");
-    td.innerHTML=keys[key];
+    td.innerHTML=keys[i];
     var td2 = document.createElement("td");
-    td2.innerHTML=values[key];
+    td2.innerHTML=values[i];
     tr.appendChild(td);
     tr.appendChild(td2);
     t.appendChild(tr);
