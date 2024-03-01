030ab8267cf5c90c0185af4a5705c4367161d473
Nzhg3i-Nzm <136181661+Nzhg3i-Nzm@users.noreply.github.com>
2024-03-01 12:01:27 -0600

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -13,7 +13,7 @@ async function createTable(){
 
   for (key in keys){
     //get all keys
-    values.push(vulns.keys[key]);
+    values.push(vulns.keys[key].toString());
   }
 
   for (let i=0; i<keys.length; i++){
