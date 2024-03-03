99e9283f2695d767b509ec1cae826334bf4d7434
Nzhg3i-Nzm <136181661+Nzhg3i-Nzm@users.noreply.github.com>
2024-03-02 20:33:28 -0600

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -15,7 +15,7 @@ function makeNavbar(parent, child){
     let l = "../../"+loc[w];
     button.setAttribute("onclick", "window.location.href='"+l+"'");
     let text = document.createElement("p");
-    text.setAttribute("style", "display: inline; font-family: 'Lucida Console'");
+    text.setAttribute("style", "display: inline; font-family: 'Kode Mono'");
     text.innerHTML=words[w];
     button.append(text);
     c.append(button)
