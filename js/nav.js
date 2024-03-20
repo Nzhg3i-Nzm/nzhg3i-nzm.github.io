57d90246bfee4d3e82405d10a7ca5f07a2826688
Nzhg3i-Nzm <136181661+Nzhg3i-Nzm@users.noreply.github.com>
2024-03-20 10:31:34 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -9,7 +9,7 @@ function makeNavbar(parent, child){
   let navdiv = document.createElement("img");
   navdiv.setAttribute("id", "navbar");
   navdiv.setAttribute("src", "../../images/navimage.jpg");
-  navdiv.setAttribute("style", "visibility: shown; z-index: 9; position: absolute; border-radius: 10px; width: 1500px; height: 750px;");
+  navdiv.setAttribute("style", "visibility: shown; z-index: 9; position: absolute; border-radius: 10px; width: 375px; height: 187.5px;");
   navdiv.setAttribute("class", "full-nav");
   let c = document.createElement("center");
   for (w in words){
